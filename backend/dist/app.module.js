5f2892f55139b5a6de9bad98b5e3eef0e8fdd989
mark1yankits <marcusik6509@gmail.com>
2025-04-23 14:27:16 +0300

    fix becend json file

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -1,46 +1,31 @@
-'use strict';
-var __decorate =
-	(this && this.__decorate) ||
-	function (decorators, target, key, desc) {
-		var c = arguments.length,
-			r =
-				c < 3
-					? target
-					: desc === null
-						? (desc = Object.getOwnPropertyDescriptor(target, key))
-						: desc,
-			d;
-		if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
-			r = Reflect.decorate(decorators, target, key, desc);
-		else
-			for (var i = decorators.length - 1; i >= 0; i--)
-				if ((d = decorators[i]))
-					r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-		return c > 3 && r && Object.defineProperty(target, key, r), r;
-	};
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
-const common_1 = require('@nestjs/common');
-const typeorm_1 = require('@nestjs/typeorm');
-const database_service_1 = require('./database/database.service');
-const train_module_1 = require('./train/train.module');
-const auth_module_1 = require('./auth/auth.module');
-const users_module_1 = require('./users/users.module');
-let AppModule = class AppModule {};
+const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const database_service_1 = require("./database/database.service");
+const train_module_1 = require("./train/train.module");
+const auth_module_1 = require("./auth/auth.module");
+const users_module_1 = require("./users/users.module");
+let AppModule = class AppModule {
+};
 exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate(
-	[
-		(0, common_1.Module)({
-			imports: [
-				typeorm_1.TypeOrmModule.forRootAsync({
-					useClass: database_service_1.DatabaseService,
-				}),
-				train_module_1.TrainModule,
-				auth_module_1.AuthModule,
-				users_module_1.UsersModule,
-			],
-			providers: [database_service_1.DatabaseService],
-		}),
-	],
-	AppModule,
-);
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            typeorm_1.TypeOrmModule.forRootAsync({
+                useClass: database_service_1.DatabaseService,
+            }),
+            train_module_1.TrainModule,
+            auth_module_1.AuthModule,
+            users_module_1.UsersModule,
+        ],
+        providers: [database_service_1.DatabaseService],
+    })
+], AppModule);
